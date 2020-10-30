3f584aa14181e7b05bbdee0ecfc700f62761402e
abelLap <abeltrece@gmail.com>
2020-10-29 23:00:06 -0600

    first commit

---
+++ app/src/main/java/com/example/pagomovil/Vista/Venta/Venta.java
@@ -0,0 +1,16 @@
+package com.example.pagomovil.Vista.Venta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pagomovil.R;
+
+public class Venta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_venta);
+    }
+}
